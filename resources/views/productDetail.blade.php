c3b5a14ab3896bc9b685425fc627bcbe360ca5d1
Angogi <andresmahanaim@gmail.com>
2019-10-25 15:01:56 +0200

    cambios aceptados

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<h1>:::: {{$productDetails->name}} ::::</h1>
-<br>
-<h2>Description</h2>
-<br>
-{{$productDetails->description}}
-<br>
-<h3>Last update</h3>
-<br>
-{{$productDetails->created_at}}
-
-<form action="{{route('profile.edit',$profileDetails->id)}}" method="get">
-    @csrf
-    <input type="submit" class="btn btn-outline-primary btn-sm " value="Edit">
-</form>
-
-=======
 @extends('layouts.app')
 
 @section('content')
@@ -60,4 +43,3 @@
 </div>
 
 @endsection
->>>>>>> 0f863689a3a85f16e4260b15a319a24ab5e986c9
