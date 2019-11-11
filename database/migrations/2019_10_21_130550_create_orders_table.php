d8cbd1590c2a1ad11009eb673345d7d21fd93276
Angogi <andresmahanaim@gmail.com>
2019-11-11 10:35:09 +0100

    migrations and foreing done

--- database/migrations/2019_10_21_130550_create_orders_table.php
+++ database/migrations/2019_10_21_130550_create_orders_table.php
@@ -17,23 +17,15 @@ class CreateOrdersTable extends Migration
 
 
             $table->bigIncrements('id');
-<<<<<<< HEAD
-            $table->bigInteger('seller_id')->unsigned()->nullble();
-            $table->bigInteger('buyer_id')->unsigned()->nullble();
-            $table->bigInteger('status_id')->unsigned()->nullable();
-            $table->unsignedBigInteger('product_id')->nullble();
+            $table->BigInteger('product_id')->nullble();
             $table->double('price')->nullable();
-            $table->date('creation_date');
+            $table->bigInteger('status_id')->unsigned();
+            $table->date('creation_date')->nullable();
 
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('price')->references('final_price')->on('products');
+            // $table->foreign('product_id')->references('id')->on('products');
+            // $table->foreign('price')->references('final_price')->on('products');
             
         
-=======
-            $table->bigInteger('seller_id')->nullable();
-            $table->date('creation_date')->nullable();
-            $table->double('total_price')->nullable();
->>>>>>> develop
         });
 
         
