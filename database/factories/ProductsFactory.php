7ef3c67b1cb302999e9f05f7292a5133ff9542e5
Carlos Pinilla <carlos.pinilla.m@gmail.com>
2019-11-11 16:25:58 +0100

    seeders done

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Products::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'initial_price' => Int::random(50),
+        'initial_price' => random_int(1, 50),
         'user_id' => 3,
     ];
 });
