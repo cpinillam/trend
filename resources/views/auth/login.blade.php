42a0dca152d57b3a3f1e1b77d9cb111b449cd678
Devilzac <shane199274@gmail.com>
2019-11-20 10:24:44 +0100

    added new css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -13,12 +13,12 @@
 
                         <div class="form-group row">
                           
-                            <div class="col-md-6">
+                            <div class="col-12">
                                 <div class="likit-container">
                                     <div  class="icon-container">
                                         <i class="fas fa-envelope"></i>
                                     </div>
-                                    <input id="email" type="email" class="likit-input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" placeholder="Email" class="likit-input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 </div>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -30,12 +30,12 @@
 
                         <div class="form-group row">                        
 
-                            <div class="col-md-6">
+                            <div class="col-12">
                                 <div class="likit-container">
                                     <div  class="icon-container">
                                         <i class="fas fa-lock"></i>
                                     </div>
-                                    <input id="password" type="password" class="likit-input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password" placeholder="Password" class="likit-input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 </div>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -45,31 +45,32 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
 
                         <div class="form-group row mb-0 buttonGrandParent">
-                            <div class="col-md-8 offset-md-3 buttonParent">
-                                <button type="submit" class="singInButton col-md-8 offset-md-2">
+                            <div class="col-12 buttonParent">
+                                   <div class="col-12">
+                                        <div class="">
+                                            <input class="col-6 form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+
+
+                                <button type="submit" class="singInButton col-12">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="col-12" class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
