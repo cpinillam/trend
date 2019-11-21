7ca35bd2b428ef60570c67a913a91c7c2fb04cde
Devilzac <shane199274@gmail.com>
2019-11-21 18:37:00 +0100

    added images products inside profile

--- resources/views/_productAndOrderProfile.blade.php
+++ resources/views/_productAndOrderProfile.blade.php
@@ -1,16 +1,21 @@
 <hr>
 <ul class="card-header nav nav-pills">
-    <li class="active"><a data-toggle="pill" class="likit-border-button" href="#myProducts">MY PRODUCTS</a></li>
+    <li class="active"><a data-toggle="pill" class="likit-border-button" href="#myProducts">My Products</a></li>
 
     @can('edit',$profileDetails)
-        <li class=""><a data-toggle="pill" class="likit-border-button" href="#myOrders">MY Orders</a></li>
+        <li class=""><a data-toggle="pill" class="likit-border-button" href="#myOrders">My Orders</a></li>
     @endcan
 </ul>
 <div class="card-header tab-content">
     <div id="myProducts" class="card-header tab-pane fade in active show"> 
         <div name="product" id="product" class="" style="display: flex; flex-wrap:wrap;justify-content:flex-start;">
-        @foreach($userProducts as $product)
-            <form action="../product/{{$product->id}}" method='get'><input type='submit' value="{{$product->sellable ? $product->name :  "#$product->id VENDIDO"}}" class="btn {{$product->sellable ? 'btn-success' : 'btn-danger'}}" style='text-align:center;border-radius:5px;margin: 5px; padding: 38px 0; width: 100px;'></form>
+        @foreach($userProducts as $index => $product)
+        <div class="product-container col-4" style="position: relative; padding:15px;">
+                       
+            <form action="../product/{{$product->id}}" method='get'>
+            <img class="img-product" src="{{asset($myProductsImage[$index])}}"> 
+                <input type='submit' class="{{$product->sellable ? 'novendido' :  'vendido'}}" value="{{$product->sellable ? $product->name :  "#$product->id VENDIDO"}}" class="btn" ></form>
+        </div>
         @endforeach
         </div>
     </div>
@@ -22,7 +27,13 @@
         @if($myorders->count() >= 1)
             @can('isOwnerOfProduct', $myorders[0])
                 @foreach($myorders as $order)
-                    <form action="../product/{{$order->product_id}}" method='get'><input type='submit' value="{{$order->product->name}}" class="btn btn-success" style='text-align:center;border-radius:5px;margin: 5px; padding: 38px 0; width: 100px;'></form>
+                    <form action="../product/{{$order->product_id}}" method='get'>
+                
+                    <input type='submit' value="{{$order->product->name}}" class="btn btn-success" style='text-align:center;border-radius:5px;margin: 5px; padding: 38px 0; width: 100px;'>
+
+                    
+                               
+                </form>
                 @endforeach
             @endcan
         @endif
