ce188a6dcf2d862c09e2ab9efd37c066ab9b454b
Devilzac <shane199274@gmail.com>
2019-11-21 13:22:50 +0100

    add style new product form

--- resources/views/products/products-new-form.blade.php
+++ resources/views/products/products-new-form.blade.php
@@ -4,75 +4,76 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Create New Product</div>
+                <h1 class="likit-h1">Create New Product</h1>
 
-                <div class="card-body">
                     
                     <form method="POST" action="/product">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Product Name') }}</label>
-
-                            <div class="col-md-6">
-
+                            <div class="col-12">
                                 <!-- To Do Ask Value into a INPUT -->
-                                <input type="number" name="user_id" id="user_id" value="{{ Auth::user()->id }}" hidden>
+                                <input  type="number" name="user_id" id="user_id" value="{{ Auth::user()->id }}" hidden>
                                 <!--  ----->
 
 
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <div class="likit-container likit-margin-Bottom">
+                                        <input placeholder="Product Name" id="name" type="text" class="col-12 likit-input @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                                        <div  class="icon-container">
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
                         </div>
 
 
                         <div class="form-group row">
-                            <label for="initial_price" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+                            <div class="col-12">
+                                <div class="likit-container likit-margin-Bottom">
+                                    <input placeholder="Price" id="initial_price" type="number" class="col-12 likit-input @error('initial_price') is-invalid @enderror" name="initial_price" value="{{ old('initial_price') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                                               
-                                <input id="initial_price" type="number" class="form-control @error('initial_price') is-invalid @enderror" name="initial_price" value="{{ old('initial_price') }}" required autofocus>
-
-                                @error('initial_price')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-
-                        <div class="form-group row">
-                                <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
-    
-                                <div class="col-md-6">
-                                                                   
-                                    <input id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autofocus>
-    
-                                    @error('description')
+                                        <div  class="icon-container">
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                    @error('initial_price')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
                             </div>
+                        </div>
 
 
-                        <button type="submit" class="btn btn-primary">
+                        <div class="form-group row">
+                                <div class="col-12">
+                                    <div class="likit-container likit-margin-Bottom">
+                                        <input placeholder="Description" id="description" type="text" class="col-12 likit-input @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autofocus>
+        
+                                        <div  class="icon-container">
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                        @error('description')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        <button type="submit" class="likit-forms-Button col-12">
                                 Enviar
-                            </button>
+                        </button>
 
                     </form>    
-                  
-                </div>
-            </div>
         </div>
     </div>
 </div>
