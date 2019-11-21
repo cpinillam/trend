6353d0788c3943d84871ea7fccbba8b1f4f284eb
Carlos Pinilla <carlos.pinilla.m@gmail.com>
2019-11-21 20:31:51 +0100

    preparing banner

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+
+<div class="container col-sm-12">
+
+
+</div>
+
 <div class="container col-sm-12">
     <div class="row justify-content-center">
         <div class="col-md-10 col-lg-8 col-sm-12">
--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -4,6 +4,8 @@
 
 
 <div class="container">
+
+ 
     <div class="row justify-content-center">
         <div class="col-md-8">
                 <h1 class="likit-h1">Profile editor</h1>
